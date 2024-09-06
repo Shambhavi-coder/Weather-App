f00f2f107c8a44fed44b20001d68fd8bcf91a9c8
Shambbhavi-coder <shambhavi3jul@gmail.com>
2024-09-06 08:57:50 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,162 @@
+const apiKey = "7d5e74e7b112e34001dc87b79a2fc7c3";
+const apiUrl = "https://api.openweathermap.org/data/2.5/";
+
+// Selectors
+const cityInput = document.querySelector('.city-input');
+const searchBtn = document.querySelector('.search-btn');
+const weatherInfoSection = document.querySelector('.weather-info');
+const notFoundSection = document.querySelector('.not-found');
+const searchCitySection = document.querySelector('.search-city');
+const countryTxt = document.querySelector('.country-txt');
+const tempTxt = document.querySelector('.temp-txt');
+const conditionTxt = document.querySelector('.condition-txt');
+const humidityValueTxt = document.querySelector('.humidity-value-txt');
+const windValueTxt = document.querySelector('.wind-value-txt');
+const weatherSummaryImg = document.querySelector('.weather-summary-img');
+const currentDateTxt = document.querySelector('.current-date-txt');
+const forecastItemsContainer = document.querySelector('.forecast-items-container');
+
+// Event listeners
+searchBtn.addEventListener('click', () => {
+    if (cityInput.value.trim() !== '') {
+        updateWeatherInfo(cityInput.value);
+        cityInput.value = '';
+        cityInput.blur();
+    }
+});
+
+cityInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter' && cityInput.value.trim() !== '') {
+        updateWeatherInfo(cityInput.value);
+        cityInput.value = '';
+        cityInput.blur();
+    }
+});
+
+async function getFetchData(endPoint, city) {
+    const url = `${apiUrl}${endPoint}?q=${city}&appid=${apiKey}&units=metric`;
+    const response = await fetch(url);
+    return response.json();
+}
+
+function getWeatherIcon(id) {
+    if (id <= 232) return 'thunderstorm.svg';
+    if (id <= 321) return 'drizzle.svg';
+    if (id <= 531) return 'rain.svg';
+    if (id <= 622) return 'snow.svg';
+    if (id <= 781) return 'atmosphere.svg';
+    if (id <= 800) return 'clear.svg';
+    return 'clouds.svg';
+}
+
+function getCurrentDate() {
+    const currentDate = new Date();
+    const options = {
+        weekday: 'short',
+        day: '2-digit',
+        month: 'short'
+    };
+    return currentDate.toLocaleDateString('en-GB', options);
+}
+
+async function updateWeatherInfo(city) {
+    const weatherData = await getFetchData('weather', city);
+    
+    if (weatherData.cod !== 200) {
+        showDisplaySection(notFoundSection);
+        return;
+    }
+    
+    const {
+        name: country,
+        main: { temp, humidity },
+        weather: [{ id, main }],
+        wind: { speed }
+    } = weatherData;
+
+    countryTxt.textContent = country;
+    tempTxt.textContent = Math.round(temp) + ' °C';
+    
+    // Update conditionTxt based on the weather condition
+    conditionTxt.textContent = getWeatherDescription(main);
+    
+    humidityValueTxt.textContent = humidity + '%';
+    windValueTxt.textContent = speed + ' m/s';
+
+    currentDateTxt.textContent = getCurrentDate();
+    weatherSummaryImg.src = `assets/weather/${getWeatherIcon(id)}`;
+
+    await updateForecastsInfo(city);
+    showDisplaySection(weatherInfoSection);
+}
+
+function getWeatherDescription(weatherCondition) {
+    switch (weatherCondition) {
+        case "Clear":
+            return "Clear Sky";
+        case "Clouds":
+            return "Cloudy";
+        case "Rain":
+            return "Rainy";
+        case "Drizzle":
+            return "Drizzle";
+        case "Snow":
+            return "Snowy";
+        case "Thunderstorm":
+            return "Thunderstorm";
+        case "Mist":
+            return "Mist";
+        default:
+            return "Weather condition not available";
+    }
+}
+
+async function updateForecastsInfo(city) {
+    const forecastsData = await getFetchData('forecast', city);
+
+    const timeTaken = '12:00:00';
+    const todayDate = new Date().toISOString().split('T')[0];
+
+    forecastItemsContainer.innerHTML = '';
+    forecastsData.list.forEach(forecastWeather => {
+        if (forecastWeather.dt_txt.includes(timeTaken) &&
+            !forecastWeather.dt_txt.includes(todayDate)) {
+            updateForecastItems(forecastWeather);
+        }
+    });
+}
+
+function updateForecastItems(weatherData) {
+    const {
+        dt_txt: date,
+        weather: [{ id }],
+        main: { temp }
+    } = weatherData;
+
+    const dateTaken = new Date(date);
+    const dateOption = {
+        day: '2-digit',
+        month: 'short'
+    };
+    const dateResult = dateTaken.toLocaleDateString('en-US', dateOption);
+
+    const forecastItem = `
+        <div class="forecast-item">
+            <h5 class="forecast-item-date regular-txt">${dateResult}</h5>
+            <img src="assets/weather/${getWeatherIcon(id)}" class="forecast-item-img">
+            <h5 class="forecast-item-temp">${Math.round(temp)} °C</h5>
+        </div>
+    `;
+
+    forecastItemsContainer.insertAdjacentHTML('beforeend', forecastItem);
+}
+
+function showDisplaySection(section) {
+    [weatherInfoSection, searchCitySection, notFoundSection]
+        .forEach(sec => sec.style.display = 'none');
+
+    section.style.display = 'flex';
+}
+
+// Optional initial check (uncomment if needed)
+// updateWeatherInfo('Jakarta');
